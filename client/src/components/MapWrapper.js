c1495445ae4739e9e171b3da57eda7d0edeaff22
Mattia Verzaroli <mattia.verzaroli@students.fhnw.ch>
2024-04-04 10:04:40 +0200

    modified:   client/src/App.css renamed:    client/src/Image/Luftbild.png -> client/src/Image/Bild_Luftbild.png renamed:    client/src/Image/osm.png -> client/src/Image/Bild_osm.png modified:   client/src/components/MapWrapper.js

--- client/src/components/MapWrapper.js
+++ client/src/components/MapWrapper.js
@@ -14,12 +14,16 @@ import { DragRotateAndZoom, defaults as defaultInteractions } from 'ol/interacti
 import { FullScreen, ScaleLine, defaults as defaultControls } from 'ol/control.js';
 import './MapWrapper.css';
 import LayersIcon from '@mui/icons-material/Layers';
+import Landeskarte_farbe from '../Image/Landeskarte_farbe.png';
+import Landeskarte_grau from '../Image/Landeskarte_grau.png';
+import Bild_Luftbild from '../Image/Bild_Luftbild.png';
+import Bild_osm from '../Image/Bild_osm.png'
 
 function MapWrapper(props) {
   const [map, setMap] = useState();
   const [featuresLayer, setFeaturesLayer] = useState();
   const [selectedCoord, setSelectedCoord] = useState();
-  const [backgroundMap, setBackgroundMap] = useState('Landeskarte farbe'); // Impostazione predefinita della mappa
+  const [backgroundMap, setBackgroundMap] = useState('Landeskarte-farbe'); // Impostazione predefinita della mappa
   const [menuOpen, setMenuOpen] = useState(false);
   const [currentView, setCurrentView] = useState(null);
   const desktopMinZoom = 8.3;
@@ -87,7 +91,7 @@ function MapWrapper(props) {
 
   const handleMapClick = (event) => {
     const clickedCoord = mapRef.current.getCoordinateFromPixel(event.pixel);
-    //const transormedCoord = transform(clickedCoord, 'EPSG:4326', 'EPSG:3857');
+    //const transformedCoord = transform(clickedCoord, 'EPSG:4326', 'EPSG:3857');
     //setSelectedCoord(transormedCoord);
     //console.log(transormedCoord);
     setSelectedCoord(clickedCoord);
@@ -99,7 +103,7 @@ function MapWrapper(props) {
       return new TileLayer({
         source: new OSM()
       });
-    } else if (backgroundMap === 'Landeskarte farbe') {
+    } else if (backgroundMap === 'Landeskarte-farbe') {
       return new TileLayer({
         source: new TileWMS({
           url: 'https://wms.geo.admin.ch/',
@@ -152,7 +156,7 @@ function MapWrapper(props) {
     // Define the extent based on the background map
     if (backgroundMap === 'osm') {
       return [506943.5, 5652213.5, 1301728.5, 6191092]; // boundigbox for EPSG:3857 (Web Mercator)
-    } else if (backgroundMap === 'Landeskarte farbe') {
+    } else if (backgroundMap === 'Landeskarte-farbe') {
       return [506943.5, 5652213.5, 1301728.5, 6191092];
     } else if (backgroundMap === 'Landeskarte-grau') {
       return [506943.5, 5652213.5, 1301728.5, 6191092];
@@ -174,6 +178,8 @@ function MapWrapper(props) {
       map.getView().fit(extent, map.getSize());
     };
 
+    toggleMenu();
+
     // Ripristina la vista corrente dopo il cambio dello sfondo
     if (currentView) {
       map.getView().setCenter(currentView.center);
@@ -185,6 +191,22 @@ function MapWrapper(props) {
     setMenuOpen(!menuOpen);
   };
 
+  
+  const backgroundImageSrc = (() => {
+    switch (backgroundMap) {
+      case 'Landeskarte-farbe':
+        return Landeskarte_farbe;
+      case 'Landeskarte-grau':
+        return Landeskarte_grau;
+      case 'Luftbild':
+        return Bild_Luftbild;
+      case 'osm':
+        return Bild_osm;
+      default:
+        return Landeskarte_farbe;
+    }
+  })();
+
   return (
     <div>
     <div className="map-fullscreen-container">
@@ -194,7 +216,7 @@ function MapWrapper(props) {
       </div>
     </div>
     <div className="background-container" onClick={toggleMenu}>
-      <LayersIcon />
+      <img src={backgroundImageSrc} width="50" height="50" onClick={toggleMenu} />
     </div>
     {menuOpen && (
     <div className="background-select">
@@ -202,19 +224,19 @@ function MapWrapper(props) {
       <div>
         <div onClick={() => handleBackgroundChange({ target: { value: 'Landeskarte-farbe' } })}>
           <p>Landeskarte farben</p>
-          <img src="../Image/Landeskarte_farbe.png" width="50" height="50" />
+          <img src={Landeskarte_farbe} width="50" height="50" />
         </div>
         <div onClick={() => handleBackgroundChange({ target: { value: 'Landeskarte-grau' } })}>
           <p>Landeskarte grau</p>
-          <img src="../Image/Landeskarte_grau.png" width="50" height="50" />
+          <img src={Landeskarte_grau} width="50" height="50" />
         </div>
         <div onClick={() => handleBackgroundChange({ target: { value: 'Luftbild' } })}>
           <p>Luftbild</p>
-          <img src="../Image/Luftbild.png" width="50" height="50" />
+          <img src={Bild_Luftbild} width="50" height="50" />
         </div>
         <div onClick={() => handleBackgroundChange({ target: { value: 'osm' } })}>
           <p>OpenStreetMap</p>
-          <img src="../Image/osm.png" width="50" height="50" />
+          <img src={Bild_osm} width="50" height="50" />
         </div>
       </div>
     </div>
