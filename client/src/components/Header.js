77d9b25d47bf5bac63191d8fd1da1a50ef8bc46a
Mattia Verzaroli <mattia.verzaroli@students.fhnw.ch>
2024-04-30 10:43:31 +0200

    modified:   client/src/App.css 	modified:   client/src/components/Header.js 	deleted:    client/src/components/main.js

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import '../App.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <div className="header">
       <div className="app-label">
         <p>ÖV-Now</p>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Karte</Link>
+              </li>
+              <li>
+                <Link to="/info-page">Info Page</Link>
+              </li>
+            </ul>
+          </nav>
       </div>
     </div>
   );
