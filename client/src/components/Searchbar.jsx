0ea3268eb9579b18fb406347a38cf194522646c6
Mattia Verzaroli <mattia.verzaroli@students.fhnw.ch>
2024-05-20 14:47:23 +0200

    modified:   client/src/components/Layers.css 	modified:   client/src/components/Searchbar.css 	modified:   client/src/components/Searchbar.jsx

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './Searchbar.css'
+import SearchIcon from '@mui/icons-material/Search';
 
 const Searchbar = ({ onSearch }) => {
   const [searchTerm, setSearchTerm] = useState('');
@@ -10,14 +11,17 @@ const Searchbar = ({ onSearch }) => {
   };
 
   return (
-    <div>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
       <input
         type="text"
         placeholder="Bahnhof"
+        style={{ height: '28px', width: '30vw' }}
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
       />
-      <button onClick={handleSearch}>Cerca</button>
+      <button onClick={handleSearch}>
+        <SearchIcon />
+      </button>
     </div>
   );
 };
