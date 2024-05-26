6fb8af58830b9382ed9c35ad8a08f5c06008e706
Mattia Verzaroli <mattia.verzaroli@students.fhnw.ch>
2024-05-26 13:52:27 +0200

    modified:   client/package-lock.json 	modified:   client/package.json 	modified:   client/src/components/Layers.jsx 	modified:   client/src/components/MapWrapper.jsx 	modified:   client/src/components/Searchbar.css 	modified:   client/src/components/Searchbar.jsx

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,29 +1,39 @@
 import React, { useState } from 'react';
-import './Searchbar.css'
+import './Searchbar.css';
 import SearchIcon from '@mui/icons-material/Search';
 
-const Searchbar = ({ onSearch }) => {
-  const [searchTerm, setSearchTerm] = useState('');
+const Searchbar = ({ searchLine }) => {
+    const [inputValue, setInputValue] = useState('');
 
-  const handleSearch = () => {
-    // Passa il termine di ricerca al genitore tramite la funzione onSearch
-    onSearch(searchTerm);
-  };
+    const handleInputChange = (event) => {
+        setInputValue(event.target.value);
+    };
 
-  return (
-    <div style={{ display: 'flex', alignItems: 'center' }}>
-      <input
-        type="text"
-        placeholder="Bahnhof"
-        style={{ height: '28px', width: '30vw' }}
-        value={searchTerm}
-        onChange={(e) => setSearchTerm(e.target.value)}
-      />
-      <button onClick={handleSearch}>
-        <SearchIcon />
-      </button>
-    </div>
-  );
+    const handleSearch = () => {
+        if (inputValue.trim()) {
+            searchLine('line_name', inputValue);
+        }
+    };
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            handleSearch();
+        }
+    };
+
+    return (
+        <div className="searchbar-container">
+            <input
+                type="text"
+                id="search-input"
+                placeholder="Ricerca di indirizzi, parcelle o mappe"
+                value={inputValue}
+                onChange={handleInputChange}
+                onKeyDown={handleKeyDown}
+            />
+            <SearchIcon className="search-icon" />
+        </div>
+    );
 };
 
 export default Searchbar;
